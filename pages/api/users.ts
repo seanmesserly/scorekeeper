3743d98e3d7471044b70894de7bff70c98e09355
Justin Knueppel <justinknueppel@gmail.com>
2021-09-12 23:53:53 -0400

    Add ability to create user

--- pages/api/users.ts
+++ pages/api/users.ts
@@ -5,6 +5,32 @@ export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const posts = await prisma.course.findMany();
-  res.json(posts);
+  if (req.method === "POST") {
+    const { firstName, lastName, email } = req.body;
+    if (
+      !firstName ||
+      !lastName ||
+      !email ||
+      typeof firstName !== "string" ||
+      typeof lastName !== "string" ||
+      typeof email !== "string"
+    ) {
+      return res.status(400).end();
+    }
+
+    const userWithEmail = await prisma.user.findFirst({
+      where: { email: email },
+    });
+    if (userWithEmail) {
+      return res.status(409).end();
+    }
+
+    const user = await prisma.user.create({
+      data: { fName: firstName, lname: lastName, email: email },
+    });
+
+    return res.status(201).json(user);
+  }
+
+  res.status(404).end();
 }
