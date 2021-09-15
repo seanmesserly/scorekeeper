c2a65df264fd7d661d93bc64eab7e3c2666acacc
seanmesserly <messerly.38@osu.edu>
2021-09-15 19:44:55 -0400

    fixed some warnings

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -19,9 +19,9 @@ const Searchbar = () => {
           stroke="currentColor"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
           />
         </svg>
@@ -42,11 +42,8 @@ const Searchbar = () => {
         {results
           .filter((result) => result.includes(input))
           .map((result) => (
-            <Link href={`/course/${result}`}>
-              <li
-                className="px-5 py-3 text-gray-600 hover:bg-gray-100 font-semibold cursor-pointer"
-                key={result}
-              >
+            <Link href={`/course/${result}`} key={result}>
+              <li className="px-5 py-3 text-gray-600 hover:bg-gray-100 font-semibold cursor-pointer">
                 <a>{result}</a>
               </li>
             </Link>
