33d9d6bb7a52d1d74d3f8a78c9ab10c91ca3279e
Justin Knueppel <justinknueppel@gmail.com>
2021-10-15 11:55:52 -0400

    Add get specific layout

---
+++ pages/api/courses/[courseId]/layouts/[layoutId]/index.ts
@@ -0,0 +1,46 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../../../../lib/prisma";
+import { getNumericId } from "../../../../../../lib/util";
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const courseId = getNumericId(req.query.courseId);
+  if (!courseId) {
+    return res.status(404).end();
+  }
+  const layoutId = getNumericId(req.query.layoutId);
+  if (!layoutId) {
+    return res.status(404).end();
+  }
+
+  if (req.method === "GET") {
+    const course = await prisma.course.findUnique({
+      where: { id: courseId },
+    });
+    if (!course) {
+      return res.status(404).end();
+    }
+    const layout = await prisma.layout.findUnique({
+      where: { id: layoutId },
+      include: { holes: true },
+    });
+
+    return res.status(200).json({
+      layout: {
+        id: layout.id,
+        name: layout.name,
+        holes: layout.holes.map((hole) => {
+          return {
+            number: hole.number,
+            par: hole.par,
+            distance: hole.distance,
+          };
+        }),
+      },
+    });
+  }
+
+  return res.status(404).end();
+}
