15bd2734bcef9e52a6cb0f09e7e917da78ad1341
Justin Knueppel <justinknueppel@gmail.com>
2021-10-15 11:59:52 -0400

    Add layout put method

--- pages/api/courses/[courseId]/layouts/[layoutId]/index.ts
+++ pages/api/courses/[courseId]/layouts/[layoutId]/index.ts
@@ -2,6 +2,38 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../../../../lib/prisma";
 import { getNumericId } from "../../../../../../lib/util";
 
+interface HoleSchema {
+  number: number;
+  par: number;
+  distance: number;
+}
+
+function isHole(object: any): boolean {
+  return (
+    object.number &&
+    typeof object.number === "number" &&
+    object.par &&
+    typeof object.par === "number" &&
+    object.distance &&
+    typeof object.distance === "number"
+  );
+}
+
+interface PutBody {
+  name: string;
+  holes: Array<HoleSchema>;
+}
+
+function isPutBody(object: any): boolean {
+  return (
+    object.name &&
+    typeof object.name === "string" &&
+    object.holes &&
+    object.holes instanceof Array &&
+    object.holes.every((hole) => isHole(hole))
+  );
+}
+
 export default async function handle(
   req: NextApiRequest,
   res: NextApiResponse
@@ -39,6 +71,54 @@ export default async function handle(
             number: hole.number,
             par: hole.par,
             distance: hole.distance,
+          };
+        }),
+      },
+    });
+  } else if (req.method === "PUT") {
+    const course = await prisma.course.findUnique({
+      where: { id: courseId },
+    });
+    if (!course) {
+      return res.status(404).end();
+    }
+    const layout = await prisma.layout.findUnique({
+      where: { id: layoutId },
+      include: { holes: true },
+    });
+    if (!layout) {
+      return res.status(404).end();
+    }
+
+    if (!isPutBody(req.body)) {
+      return res.status(400).json({ error: "Invalid input" });
+    }
+
+    const { name, holes } = req.body as PutBody;
+
+    const updatedLayout = await prisma.layout.update({
+      where: { id: layout.id },
+      data: {
+        name: name,
+        holes: {
+          create: holes,
+        },
+      },
+    });
+
+    const savedHoles = await prisma.hole.findMany({
+      where: { layoutId: layout.id },
+    });
+
+    return res.status(200).json({
+      layout: {
+        id: updatedLayout.id,
+        name: updatedLayout.name,
+        holes: savedHoles.map((hole) => {
+          return {
+            par: hole.par,
+            distance: hole.distance,
+            number: hole.number,
           };
         }),
       },
