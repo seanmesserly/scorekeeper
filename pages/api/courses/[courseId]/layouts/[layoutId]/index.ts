f5fe50a7561bbc911d12e2b44756c9cbd1a127e5
Justin Knueppel <32350916+JustinKnueppel@users.noreply.github.com>
2021-11-09 23:38:04 -0500

    Add individual layout endpoints (#20)
    * Add get specific layout
    
    * Add 404 if layout not found
    
    * Add layout put method
    
    * Add delete method for layouts

---
+++ pages/api/courses/[courseId]/layouts/[layoutId]/index.ts
@@ -0,0 +1,147 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../../../../lib/prisma";
+import { getNumericId } from "../../../../../../lib/util";
+
+interface HoleSchema {
+  number: number;
+  par: number;
+  distance: number;
+}
+
+function isHole(object: any): boolean {
+  return (
+    object.number &&
+    typeof object.number === "number" &&
+    object.par &&
+    typeof object.par === "number" &&
+    object.distance &&
+    typeof object.distance === "number"
+  );
+}
+
+interface PutBody {
+  name: string;
+  holes: Array<HoleSchema>;
+}
+
+function isPutBody(object: any): boolean {
+  return (
+    object.name &&
+    typeof object.name === "string" &&
+    object.holes &&
+    object.holes instanceof Array &&
+    object.holes.every((hole) => isHole(hole))
+  );
+}
+
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const courseId = getNumericId(req.query.courseId);
+  if (!courseId) {
+    return res.status(404).end();
+  }
+  const layoutId = getNumericId(req.query.layoutId);
+  if (!layoutId) {
+    return res.status(404).end();
+  }
+
+  if (req.method === "GET") {
+    const course = await prisma.course.findUnique({
+      where: { id: courseId },
+    });
+    if (!course) {
+      return res.status(404).end();
+    }
+    const layout = await prisma.layout.findUnique({
+      where: { id: layoutId },
+      include: { holes: true },
+    });
+    if (!layout) {
+      return res.status(404).end();
+    }
+
+    return res.status(200).json({
+      layout: {
+        id: layout.id,
+        name: layout.name,
+        holes: layout.holes.map((hole) => {
+          return {
+            number: hole.number,
+            par: hole.par,
+            distance: hole.distance,
+          };
+        }),
+      },
+    });
+  } else if (req.method === "PUT") {
+    const course = await prisma.course.findUnique({
+      where: { id: courseId },
+    });
+    if (!course) {
+      return res.status(404).end();
+    }
+    const layout = await prisma.layout.findUnique({
+      where: { id: layoutId },
+      include: { holes: true },
+    });
+    if (!layout) {
+      return res.status(404).end();
+    }
+
+    if (!isPutBody(req.body)) {
+      return res.status(400).json({ error: "Invalid input" });
+    }
+
+    const { name, holes } = req.body as PutBody;
+
+    const updatedLayout = await prisma.layout.update({
+      where: { id: layout.id },
+      data: {
+        name: name,
+        holes: {
+          create: holes,
+        },
+      },
+    });
+
+    const savedHoles = await prisma.hole.findMany({
+      where: { layoutId: layout.id },
+    });
+
+    return res.status(200).json({
+      layout: {
+        id: updatedLayout.id,
+        name: updatedLayout.name,
+        holes: savedHoles.map((hole) => {
+          return {
+            par: hole.par,
+            distance: hole.distance,
+            number: hole.number,
+          };
+        }),
+      },
+    });
+  } else if (req.method === "DELETE") {
+    const course = await prisma.course.findUnique({
+      where: { id: courseId },
+    });
+    if (!course) {
+      return res.status(404).end();
+    }
+    const layout = await prisma.layout.findUnique({
+      where: { id: layoutId },
+      include: { holes: true },
+    });
+    if (!layout) {
+      return res.status(404).end();
+    }
+
+    await prisma.layout.delete({ where: { id: layout.id } });
+
+    return res.status(204).end();
+  }
+
+  return res.status(404).end();
+}
