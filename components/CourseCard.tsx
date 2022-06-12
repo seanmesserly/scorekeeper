1e1ed463def1e1f68f497c91310e78c8cd32b1f3
Justin Knueppel <justinknueppel@gmail.com>
2022-06-12 12:51:42 -0400

    Make score card preview look nicer

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,6 +1,7 @@
 import { Course, Layout, ScoreCard } from "../lib/types";
 import LayoutPreview from "../components/LayoutPreview";
 import { useState } from "react";
+import ScoreCardPreview from "./ScoreCardPreview";
 
 interface Props {
   course: Course;
@@ -8,17 +9,17 @@ interface Props {
 
 enum Tab {
   Layouts,
-  Scores
+  Scores,
 }
 
 export default function CourseCard({ course }: Props) {
   const [tab, setTab] = useState(Tab.Layouts);
-  const selectedTabColors = "bg-purple-400 text-white"
-  const unselectedTabColors = "bg-white text-purple-400"
+  const selectedTabColors = "bg-purple-400 text-white";
+  const unselectedTabColors = "bg-white text-purple-400";
   const layouts: Array<Layout> = [
     {
       id: 1,
-      name: "blue tees",
+      name: "Blue Tees",
       holes: [
         {
           number: 1,
@@ -42,7 +43,7 @@ export default function CourseCard({ course }: Props) {
     {
       courseId: course.id,
       layoutId: 1,
-      datetime: "2011-10-05T14:48:00.000Z",
+      datetime: "2011-10-05T18:48:00.000Z",
       scores: [
         {
           number: 1,
@@ -69,59 +70,53 @@ export default function CourseCard({ course }: Props) {
       </header>
       <section>
         <div className="flex flex-row mb-4">
-          <div className={`w-1/2 max-w-sm border-2 border-purple-400 text-center cursor-pointer ${tab === Tab.Layouts ? selectedTabColors : unselectedTabColors}`}><a onClick={() => setTab(Tab.Layouts)}><div>Info</div></a></div>
-          <div className={`w-1/2 max-w-sm border-2 border-purple-400 text-center cursor-pointer ${tab === Tab.Scores ? selectedTabColors : unselectedTabColors}`}><a onClick={() => setTab(Tab.Scores)}><div>Scores</div></a></div>
+          <div
+            className={`w-1/2 max-w-sm border-2 border-purple-400 text-center cursor-pointer ${
+              tab === Tab.Layouts ? selectedTabColors : unselectedTabColors
+            }`}
+          >
+            <a onClick={() => setTab(Tab.Layouts)}>
+              <div>Info</div>
+            </a>
+          </div>
+          <div
+            className={`w-1/2 max-w-sm border-2 border-purple-400 text-center cursor-pointer ${
+              tab === Tab.Scores ? selectedTabColors : unselectedTabColors
+            }`}
+          >
+            <a onClick={() => setTab(Tab.Scores)}>
+              <div>Scores</div>
+            </a>
+          </div>
         </div>
       </section>
-      {tab === Tab.Layouts && (<section>
-        <ul>
-          {layouts.map((layout) => (
+      {tab === Tab.Layouts && (
+        <section>
+          <ul>
+            {layouts.map((layout) => (
               <li>
                 <LayoutPreview layout={layout} />
               </li>
-            )
-          )}
-        </ul>
-      </section>)}
-      {tab === Tab.Scores && (<section>
-        <ul>
-          {scores.map((scoreCard) => {
-            const date = new Date(scoreCard.datetime);
-            const month = date.toLocaleString("default", { month: "long" });
-            const dateString = `${month} ${date.getDay()}, ${date.getFullYear()}`;
-            const layout = layouts.find(
-              (layout) => layout.id === scoreCard.layoutId
-            );
-            const totalPar = layout.holes
-              .map((layout) => layout.par)
-              .reduce((total, par) => total + par);
-            const totalStrokes = scoreCard.scores
-              .map((score) => score.strokes)
-              .reduce((total, strokes) => total + strokes);
-            const finalScore = totalStrokes - totalPar;
-            // Do this better
-            let scoreColor = "bg-white-200";
-            if (finalScore > 0) {
-              scoreColor = "bg-red-200";
-            } else if (finalScore < 0) {
-              scoreColor = "bg-green-200";
-            }
-            return (
+            ))}
+          </ul>
+        </section>
+      )}
+      {tab === Tab.Scores && (
+        <section>
+          <ul>
+            {scores.map((scoreCard) => (
               <li>
-                <p>
-                  {dateString} | {layout.name} |{" "}
-                  <span className={`${scoreColor} px-1`}>
-                    {totalStrokes - totalPar}
-                  </span>
-                </p>
+                <ScoreCardPreview
+                  scoreCard={scoreCard}
+                  layouts={layouts}
+                  course={course}
+                  key={scoreCard.datetime}
+                />
               </li>
-            );
-          })}
-        </ul>
-      </section>)}
-      <section className="bg-gray-200">
-        Coordinates: {course.lat}, {course.lon}
-      </section>
+            ))}
+          </ul>
+        </section>
+      )}
     </div>
   );
 }
